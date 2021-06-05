290f38c3ee969e03bdb622d4c7eca6b48a1eafec
ches7 <chesmon1@hotmail.co.uk>
2021-06-06 00:54:08 +0100

    tab 3 started

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ class SecondTab(QWidget):
 #set up updates to global variables    
     def updatevariable(self):
         global IMODE1, IMODE2, IMODE3, IMODE4, DDE, RATIO, IDIR, IREV, PEAK, NPRINT, MAXLOOP, NCYCL
-        
+
         IMODE1 = self.nameEditIMODE1.text()
         IMODE2 = self.nameEditIMODE2.text()
         IMODE3 = self.nameEditIMODE3.text()
@@ -154,13 +154,207 @@ class ThirdTab(QWidget):
     def __init__(self):
         super().__init__()
 
-        name = QLabel("Name: ")
-        nameEdit = QLineEdit()
+#PAR1 Label and LineEdit boxes
+        self.namePAR1 = QLabel("2G/K = PAR(1): ")
+        self.nameEditPAR1 = QLineEdit()
+        self.nameEditPAR1.editingFinished.connect(self.updatevariable)
 
+#PAR2 Label and LineEdit boxes
+        self.namePAR2 = QLabel("\u03BB(0) = PAR(2): ")
+        self.nameEditPAR2 = QLineEdit()
+        self.nameEditPAR2.editingFinished.connect(self.updatevariable)
+
+#PAR3 Label and LineEdit boxes
+        self.namePAR3 = QLabel("PAR3: ")
+        self.nameEditPAR3 = QLineEdit()
+        self.nameEditPAR3.editingFinished.connect(self.updatevariable)
+
+#PAR4 Label and LineEdit boxes
+        self.namePAR4 = QLabel("PAR4: ")
+        self.nameEditPAR4 = QLineEdit()
+        self.nameEditPAR4.editingFinished.connect(self.updatevariable)
+
+#PAR5 Label and LineEdit boxes
+        self.namePAR5 = QLabel("PAR5: ")
+        self.nameEditPAR5 = QLineEdit()
+        self.nameEditPAR5.editingFinished.connect(self.updatevariable)
+
+#PAR6 Label and LineEdit boxes
+        self.namePAR6 = QLabel("PAR6: ")
+        self.nameEditPAR6 = QLineEdit()
+        self.nameEditPAR6.editingFinished.connect(self.updatevariable)
+
+#PAR7 Label and LineEdit boxes
+        self.namePAR7 = QLabel("PAR7: ")
+        self.nameEditPAR7 = QLineEdit()
+        self.nameEditPAR7.editingFinished.connect(self.updatevariable)
+
+#PAR8 Label and LineEdit boxes
+        self.namePAR8 = QLabel("PAR8: ")
+        self.nameEditPAR8 = QLineEdit()
+        self.nameEditPAR8.editingFinished.connect(self.updatevariable)
+
+#PAR9 Label and LineEdit boxes
+        self.namePAR9 = QLabel("PAR9: ")
+        self.nameEditPAR9 = QLineEdit()
+        self.nameEditPAR9.editingFinished.connect(self.updatevariable)
+
+#PAR9 Label and LineEdit boxes
+        self.namePAR9 = QLabel("PAR9: ")
+        self.nameEditPAR9 = QLineEdit()
+        self.nameEditPAR9.editingFinished.connect(self.updatevariable)
+
+#PAR10 Label and LineEdit boxes
+        self.namePAR10 = QLabel("PAR10: ")
+        self.nameEditPAR10 = QLineEdit()
+        self.nameEditPAR10.editingFinished.connect(self.updatevariable)
+
+#PAR11 Label and LineEdit boxes
+        self.namePAR11 = QLabel("PAR11: ")
+        self.nameEditPAR11 = QLineEdit()
+        self.nameEditPAR11.editingFinished.connect(self.updatevariable)
+
+#PAR12 Label and LineEdit boxes
+        self.namePAR12 = QLabel("PAR12: ")
+        self.nameEditPAR12 = QLineEdit()
+        self.nameEditPAR12.editingFinished.connect(self.updatevariable)
+
+#PAR13 Label and LineEdit boxes
+        self.namePAR13 = QLabel("PAR13: ")
+        self.nameEditPAR13 = QLineEdit()
+        self.nameEditPAR13.editingFinished.connect(self.updatevariable)
+
+#PAR14 Label and LineEdit boxes
+        self.namePAR14 = QLabel("PAR14: ")
+        self.nameEditPAR14 = QLineEdit()
+        self.nameEditPAR14.editingFinished.connect(self.updatevariable)
+
+#PAR15 Label and LineEdit boxes
+        self.namePAR15 = QLabel("PAR15: ")
+        self.nameEditPAR15 = QLineEdit()
+        self.nameEditPAR15.editingFinished.connect(self.updatevariable)
+
+#PAR16 Label and LineEdit boxes
+        self.namePAR16 = QLabel("PAR16: ")
+        self.nameEditPAR16 = QLineEdit()
+        self.nameEditPAR16.editingFinished.connect(self.updatevariable)
+
+#PAR17 Label and LineEdit boxes
+        self.namePAR17 = QLabel("PAR17: ")
+        self.nameEditPAR17 = QLineEdit()
+        self.nameEditPAR17.editingFinished.connect(self.updatevariable)
+
+#PAR21 Label and LineEdit boxes
+        self.namePAR21 = QLabel("PAR21: ")
+        self.nameEditPAR21 = QLineEdit()
+        self.nameEditPAR21.editingFinished.connect(self.updatevariable)
+
+#PAR22 Label and LineEdit boxes
+        self.namePAR22 = QLabel("PAR22: ")
+        self.nameEditPAR22 = QLineEdit()
+        self.nameEditPAR22.editingFinished.connect(self.updatevariable)
+
+#PAR23 Label and LineEdit boxes
+        self.namePAR23 = QLabel("PAR23: ")
+        self.nameEditPAR23 = QLineEdit()
+        self.nameEditPAR23.editingFinished.connect(self.updatevariable)
+
+#PAR24 Label and LineEdit boxes
+        self.namePAR24 = QLabel("PAR24: ")
+        self.nameEditPAR24 = QLineEdit()
+        self.nameEditPAR24.editingFinished.connect(self.updatevariable)
+
+#PAR25 Label and LineEdit boxes
+        self.namePAR25 = QLabel("PAR25: ")
+        self.nameEditPAR25 = QLineEdit()
+        self.nameEditPAR25.editingFinished.connect(self.updatevariable)
+
+#PAR26 Label and LineEdit boxes
+        self.namePAR26 = QLabel("PAR26: ")
+        self.nameEditPAR26 = QLineEdit()
+        self.nameEditPAR26.editingFinished.connect(self.updatevariable)
+
+
+#add objects to layout
         layout = QGridLayout()
-        layout.addWidget(name, 1, 1)
-        layout.addWidget(nameEdit, 2, 1)
+
+        layout.addWidget(self.namePAR1, 1, 1)
+        layout.addWidget(self.nameEditPAR1, 1, 2)
+        layout.addWidget(self.namePAR2, 2, 1)
+        layout.addWidget(self.nameEditPAR2, 2, 2)
+        layout.addWidget(self.namePAR3, 3, 1)
+        layout.addWidget(self.nameEditPAR3, 3, 2)
+        layout.addWidget(self.namePAR4, 4, 1)
+        layout.addWidget(self.nameEditPAR4, 4, 2)
+        layout.addWidget(self.namePAR5, 5, 1)
+        layout.addWidget(self.nameEditPAR5, 5, 2)
+        layout.addWidget(self.namePAR6, 6, 1)
+        layout.addWidget(self.nameEditPAR6, 6, 2)
+        layout.addWidget(self.namePAR7, 7, 1)
+        layout.addWidget(self.nameEditPAR7, 7, 2)
+        layout.addWidget(self.namePAR8, 8, 1)
+        layout.addWidget(self.nameEditPAR8, 8, 2)
+        layout.addWidget(self.namePAR9, 9, 1)
+        layout.addWidget(self.nameEditPAR9, 9, 2)
+        layout.addWidget(self.namePAR10, 10, 1)
+        layout.addWidget(self.nameEditPAR10, 10, 2)
+        layout.addWidget(self.namePAR11, 11, 1)
+        layout.addWidget(self.nameEditPAR11, 11, 2)
+        layout.addWidget(self.namePAR12, 12, 1)
+        layout.addWidget(self.nameEditPAR12, 12, 2)
+        layout.addWidget(self.namePAR13, 13, 1)
+        layout.addWidget(self.nameEditPAR13, 13, 2)
+        layout.addWidget(self.namePAR14, 14, 1)
+        layout.addWidget(self.nameEditPAR14, 14, 2)
+        layout.addWidget(self.namePAR15, 15, 1)
+        layout.addWidget(self.nameEditPAR15, 15, 2)
+        layout.addWidget(self.namePAR16, 16, 1)
+        layout.addWidget(self.nameEditPAR16, 16, 2)
+        layout.addWidget(self.namePAR17, 17, 1)
+        layout.addWidget(self.nameEditPAR17, 17, 2)
+        layout.addWidget(self.namePAR21, 21, 1)
+        layout.addWidget(self.nameEditPAR21, 21, 2)
+        layout.addWidget(self.namePAR22, 22, 1)
+        layout.addWidget(self.nameEditPAR22, 22, 2)
+        layout.addWidget(self.namePAR23, 23, 1)
+        layout.addWidget(self.nameEditPAR23, 23, 2)
+        layout.addWidget(self.namePAR24, 24, 1)
+        layout.addWidget(self.nameEditPAR24, 24, 2)
+        layout.addWidget(self.namePAR25, 25, 1)
+        layout.addWidget(self.nameEditPAR25, 25, 2)
+        layout.addWidget(self.namePAR26, 26, 1)
+        layout.addWidget(self.nameEditPAR26, 26, 2)
+
+
         self.setLayout(layout)
+
+#set up updates to global variables    
+    def updatevariable(self):
+        global PAR1, PAR2, PAR3, PAR4, PAR5, PAR6, PAR7, PAR8, PAR9, PAR10, PAR11, PAR12, PAR13, PAR14, PAR15, PAR16, PAR17, PAR21, PAR22, PAR23, PAR24, PAR25, PAR26 
+
+        PAR1 = self.nameEditPAR1.text()
+        PAR2 = self.nameEditPAR2.text()
+        PAR3 = self.nameEditPAR3.text()
+        PAR4 = self.nameEditPAR4.text()
+        PAR5 = self.nameEditPAR5.text()
+        PAR6 = self.nameEditPAR6.text()
+        PAR7 = self.nameEditPAR7.text()
+        PAR8 = self.nameEditPAR8.text()
+        PAR9 = self.nameEditPAR9.text()
+        PAR10 = self.nameEditPAR10.text()
+        PAR11 = self.nameEditPAR11.text()
+        PAR12 = self.nameEditPAR12.text()
+        PAR13 = self.nameEditPAR13.text()
+        PAR14 = self.nameEditPAR14.text()
+        PAR15 = self.nameEditPAR15.text()
+        PAR16 = self.nameEditPAR16.text()
+        PAR17 = self.nameEditPAR17.text()
+        PAR21 = self.nameEditPAR21.text()
+        PAR22 = self.nameEditPAR22.text()
+        PAR23 = self.nameEditPAR23.text()
+        PAR24 = self.nameEditPAR24.text()
+        PAR25 = self.nameEditPAR25.text()
+        PAR26 = self.nameEditPAR26.text()
 
 
 class FourthTab(QWidget):
@@ -194,7 +388,6 @@ class FifthTab(QWidget):
     
     def updatevariable2(self):
         with open('export.txt', 'w') as f:
-            f.writelines(repr(soil) + "\n")
             f.writelines(repr(IMODE1) + "\n")
             f.writelines(repr(IMODE2) + "\n")
             f.writelines(repr(IMODE3) + "\n")
@@ -261,7 +454,6 @@ class FifthTab(QWidget):
 #elements.soil = 0
 
 #VARIABLES
-soil = 0
 NT = 0
 
 IMODE1 = 0
